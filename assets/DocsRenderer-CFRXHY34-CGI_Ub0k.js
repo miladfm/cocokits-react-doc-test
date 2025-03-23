9196941d18df0cf662b5942a01d107533e7449dc
Milad Faghihi <miladfm@gmail.com>
2025-03-23 11:00:51 +0100

    Release

--- assets/DocsRenderer-CFRXHY34-CGI_Ub0k.js
+++
@@ -1,1 +0,0 @@
-import{D as d,d as n}from"./chunk-NUUEMKO5-BJXl5ljb.js";import"./iframe-CKGaQRfv.js";import"../sb-preview/runtime.js";import"./index-qVDZzPEr.js";import"./index-DM35cTar.js";import"./index-D2o5vtt7.js";import"./index-DrFu-skq.js";import"./react-18-CVVedRj1.js";export{d as DocsRenderer,n as defaultComponents};
