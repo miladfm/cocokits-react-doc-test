d5f991f20e139c8b6f6aa7a5a81d9eba2174ad4a
Milad Faghihi <miladfm@gmail.com>
2025-07-30 09:06:03 +0200

    Release

---
+++ assets/badge-container-DsIkreak.js
@@ -0,0 +1,43 @@
+import{j as o}from"./DocsRenderer-CFRXHY34-Ce_STi8V.js";import{r as g}from"./index-C33_amOP.js";import{i as p,u as y}from"./index-BO74zFUF.js";import{u as x}from"./use-slot-children-D1X8Xi24.js";const v=({hide:s=!1,max:e,content:t,className:l,style:m,type:u,size:c,color:h,additional:f})=>{const i=g.useMemo(()=>{if(p(e)||p(t))return!1;const n=typeof t=="number"?t:parseInt(t);return isNaN(n)||e<=0?!1:e<n},[e,t]),r=g.useMemo(()=>{if(p(t))return"";const n=parseInt(t);return i&&!isNaN(n)&&!p(e)?Math.min(n,e):t},[t,i,e]),{classNames:d,hostClassNames:b}=y({componentName:"badge",props:{type:u,size:c,color:h,additional:f},extraHostElementClassConditions:[{if:!!l,classes:()=>[l]},{if:i,classes:n=>[n.maxIndicator]},{if:!!r,classes:n=>[n.withContent]},{if:!r,classes:n=>[n.withoutContent]},{if:s,classes:n=>[n.hidden]}]});return o.jsx("div",{className:b,style:m,children:!s&&o.jsxs(o.Fragment,{children:[o.jsx("span",{className:d.content,children:r}),i&&o.jsx("span",{className:d.maxIndicator,children:"+"})]})})};v.displayName="Badge";v.__docgenInfo={description:"",methods:[],displayName:"Badge",props:{type:{required:!1,tsType:{name:"union",raw:"string | number | boolean | null",elements:[{name:"string"},{name:"number"},{name:"boolean"},{name:"null"}]},description:`The type of component.\r
+\r
+When set to \`null\`, no specific type is applied (Not event the default value).\r
+This allows for more flexible styling options if the desired size is not available in the selected theme.`},color:{required:!1,tsType:{name:"union",raw:"string | number | boolean | null",elements:[{name:"string"},{name:"number"},{name:"boolean"},{name:"null"}]},description:`The color of component.\r
+\r
+When set to \`null\`, no specific color is applied (Not event the default value).\r
+This allows for more flexible styling options if the desired size is not available in the selected theme.`},size:{required:!1,tsType:{name:"union",raw:"string | number | boolean | null",elements:[{name:"string"},{name:"number"},{name:"boolean"},{name:"null"}]},description:`The size of component.\r
+\r
+When set to \`null\`, no specific size is applied (Not event the default value).\r
+This allows for more flexible styling options if the desired size is not available in the selected theme.`},additional:{required:!1,tsType:{name:"Record",elements:[{name:"string"},{name:"union",raw:"string | number | boolean | null",elements:[{name:"string"},{name:"number"},{name:"boolean"},{name:"null"}]}],raw:"Record<string, UIBaseComponentsPropValue>"},description:`The additional properties for the component base on your favorite theme.\r
+\r
+This allows each theme to define its own unique properties or configurations not shared globally across all themes.\r
+For example, "rounded", "variant", and other custom properties.\r
+Based on the value, all necessary class names are created and appended to the host class, allowing the theme to easily select and style elements.\r
+\r
+@example\r
+<Component additional={{rounded: true, variant: 'my-variant'}}/>\r
+\r
+@internal`},max:{required:!1,tsType:{name:"number"},description:"Max value before truncation."},content:{required:!1,tsType:{name:"union",raw:"number | string | undefined | null",elements:[{name:"number"},{name:"string"},{name:"undefined"},{name:"null"}]},description:'Content to display in the badge. When "", null, or undefined, shows a dot.'},hide:{required:!1,tsType:{name:"boolean"},description:"Whether to hide the badge completely. Default is false.",defaultValue:{value:"false",computed:!1}},className:{required:!1,tsType:{name:"string"},description:"A custom class name that can be used to apply additional styles to the component."},style:{required:!1,tsType:{name:"CSSProperties"},description:"An object containing inline styles that can be used to customize the appearance of the component."}}};const T=({position:s="top-left",offset:e,radius:t="0px",children:l,className:m,style:u,type:c,size:h,color:f,additional:i})=>{const{classNames:r,hostClassNames:d}=y({componentName:"badgeContainer",props:{type:c,size:h,color:f,additional:i},extraHostElementClassConditions:[{if:!!m,classes:()=>[m]},{if:s==="top-left",classes:a=>[a.topLeft]},{if:s==="top-right",classes:a=>[a.topRight]},{if:s==="bottom-left",classes:a=>[a.bottomLeft]},{if:s==="bottom-right",classes:a=>[a.bottomRight]},{if:e&&!!e[0]&&!!e[1],classes:a=>[a.customOffset]}]}),b=e&&e[0]&&e[1]?`translate(${e[0]}, ${e[1]})`:void 0,[n,w]=x(l,"Badge");return o.jsxs("div",{className:d,style:{...u,"--cck-badge-radius":t},children:[w,o.jsx("div",{className:r.badgeWrapper,style:{transform:b},children:n})]})};T.displayName="BadgeContainer";T.__docgenInfo={description:"",methods:[],displayName:"BadgeContainer",props:{type:{required:!1,tsType:{name:"union",raw:"string | number | boolean | null",elements:[{name:"string"},{name:"number"},{name:"boolean"},{name:"null"}]},description:`The type of component.\r
+\r
+When set to \`null\`, no specific type is applied (Not event the default value).\r
+This allows for more flexible styling options if the desired size is not available in the selected theme.`},color:{required:!1,tsType:{name:"union",raw:"string | number | boolean | null",elements:[{name:"string"},{name:"number"},{name:"boolean"},{name:"null"}]},description:`The color of component.\r
+\r
+When set to \`null\`, no specific color is applied (Not event the default value).\r
+This allows for more flexible styling options if the desired size is not available in the selected theme.`},size:{required:!1,tsType:{name:"union",raw:"string | number | boolean | null",elements:[{name:"string"},{name:"number"},{name:"boolean"},{name:"null"}]},description:`The size of component.\r
+\r
+When set to \`null\`, no specific size is applied (Not event the default value).\r
+This allows for more flexible styling options if the desired size is not available in the selected theme.`},additional:{required:!1,tsType:{name:"Record",elements:[{name:"string"},{name:"union",raw:"string | number | boolean | null",elements:[{name:"string"},{name:"number"},{name:"boolean"},{name:"null"}]}],raw:"Record<string, UIBaseComponentsPropValue>"},description:`The additional properties for the component base on your favorite theme.\r
+\r
+This allows each theme to define its own unique properties or configurations not shared globally across all themes.\r
+For example, "rounded", "variant", and other custom properties.\r
+Based on the value, all necessary class names are created and appended to the host class, allowing the theme to easily select and style elements.\r
+\r
+@example\r
+<Component additional={{rounded: true, variant: 'my-variant'}}/>\r
+\r
+@internal`},position:{required:!1,tsType:{name:"union",raw:"'top-left' | 'top-right' | 'bottom-left' | 'bottom-right'",elements:[{name:"literal",value:"'top-left'"},{name:"literal",value:"'top-right'"},{name:"literal",value:"'bottom-left'"},{name:"literal",value:"'bottom-right'"}]},description:"Position of the badge.",defaultValue:{value:"'top-left'",computed:!1}},offset:{required:!1,tsType:{name:"tuple",raw:"[string, string]",elements:[{name:"string"},{name:"string"}]},description:`[horizontal, vertical] pixel adjustment for badge position.\r
+Allows control over badge positioning.\r
+@example ['10px', '20px']`},radius:{required:!1,tsType:{name:"string"},description:`Controls the corner rounding of the component. Accepts any valid CSS border-radius value\r
+including pixels (e.g. "12px"), percentages (e.g. "50%").\r
+Percentage values are relative to the component's dimensions.\r
+Useful for when the content is curved such as avatars.`,defaultValue:{value:"'0px'",computed:!1}},children:{required:!0,tsType:{name:"Array",elements:[{name:"ReactNode"}],raw:"ReactNode[]"},description:`Content to display inside the badge container with the badge component\r
+This can be any valid ReactNode, such as text, images, or other components`},className:{required:!1,tsType:{name:"string"},description:"A custom class name that can be used to apply additional styles to the component."},style:{required:!1,tsType:{name:"CSSProperties"},description:"An object containing inline styles that can be used to customize the appearance of the component."}}};export{T as B,v as a};
---
+++ assets/index.stories-BiQr7tGs.js
@@ -0,0 +1,77 @@
+import{R as r}from"./radio-group-eaNDNv57.js";import{j as o}from"./DocsRenderer-CFRXHY34-Ce_STi8V.js";import"./index-BO74zFUF.js";import"./index-C33_amOP.js";import"./menu-item-81oIYHPG.js";import"./toggle-BGIU_gbr.js";import"./react-18-B-IeCXFN.js";import"./mdx-page-with-theme-section-BwKfE3JQ.js";import{C as d,t as a,c as p,s as m,a as l,d as u,G as f,e as s,x as c}from"./control.config-BvREd7te.js";import{r as y}from"./preview.base-DeVuxgdh.js";import"./iframe-CIAUh1NQ.js";import"./index-Cghhjyi9.js";import"./index-DrFu-skq.js";import"./preview-DDfixZw_.js";import"./addon.model-Bxedznpl.js";const h="The `RadioButton` from `@cocokits` is a fundamental UI element that enables users to select one option from a predefined set of mutually exclusive choices. Radio buttons are commonly used in forms and settings where users must choose a single option from a group. This component is designed to integrate seamlessly with multiple design system themes, ensuring consistency and clarity across various application styles.",S={name:"Default",parameters:{docs:{description:{story:"Shows the default example with no additional configurations, providing an interactive example in its most basic form."}},cckAddon:{renderConditions:[s("Overview")],source:[{filename:"Source Code",language:"tsx",code:`
+                     import { RadioButton } from "@cocokits/react-components";
+  
+            export const MyComponent = () => {
+              return (
+                <>
+            <RadioButton
+              <% if (typeof type !== 'undefined') { %> type='<%= type %>' <% } %>
+              <% if (typeof color !== 'undefined') { %> color='<%= color %>' <% } %>
+              <% if (typeof size !== 'undefined') { %> size='<%= size %>' <% } %>
+              <% if (disabled) { %> disabled <% } %>
+              <% if (checked) { %> checked <% } %>
+              value='YOUR_VALUE'
+              >
+              <%= text %>
+            </RadioButton>
+                </>
+              );
+            }
+
+          `}],hasControl:!0,controls:[d("Label"),a(),p(),m(),l(),u(),f(!0)]}},render:e=>o.jsx(o.Fragment,{children:o.jsx(r,{...y(e),checked:e.cckControl.checked,disabled:e.cckControl.disabled,value:1,children:e.cckControl.text})})},E={name:"Type",parameters:{docs:{description:{story:"Displays variations in appearance and functionality, demonstrating how different types can be used to create unique button styles."}},cckAddon:{renderConditions:[c("type"),s("Overview")],source:[{filename:"Source Code",language:"tsx",code:`
+                     import { RadioButton } from "@cocokits/react-components";
+  
+            export const MyComponent = () => {
+              return (
+                <>
+          <% themeComponentConfig.type.values.map(type => { %>
+            <RadioButton type='<%= type %>' value='YOUR_VALUE'>
+              Radio Button - {type}
+            </RadioButton>
+          <% }) %>
+                </>
+              );
+            }
+
+          `}]}},render:e=>{var t;return o.jsx(o.Fragment,{children:(t=e.cckControl.themeComponentConfig.type)==null?void 0:t.values.map((n,i)=>o.jsxs(r,{type:n,value:i,checked:!0,children:["Radio Button - ",n]},i))})}},_={name:"Size",parameters:{docs:{description:{story:"The size is adjustable to suit different design needs and screen dimensions, improving both aesthetics and usability."}},cckAddon:{renderConditions:[c("size"),s("Overview")],singleControls:["type"],source:[{filename:"Source Code",language:"tsx",code:`
+                     import { RadioButton } from "@cocokits/react-components";
+  
+            export const MyComponent = () => {
+              return (
+          <>
+            <% themeComponentConfig.size.values.map((size, index) => { %>
+
+              {/* <%= size %> */}
+              <RadioButton
+                <% if (typeof type !== 'undefined') { %> type='<%= type %>' <% } %>
+                size='<%= size %>'
+                value='YOUR_VALUE'>
+                Radio Button - <%= size %>
+              </RadioButton>
+            <% }) %>
+          </>
+              );
+            }
+
+          `}],controls:[a()]}},render:e=>{var t;return o.jsx(o.Fragment,{children:(t=e.cckControl.themeComponentConfig.size)==null?void 0:t.values.map((n,i)=>o.jsxs(r,{type:e.cckControl.type,size:n,value:i,checked:!0,children:["Radio Button - ",n]},i))})}},L={name:"Color",parameters:{docs:{description:{story:"Color options enable seamless integration with various themes or to highlight specific actions."}},cckAddon:{renderConditions:[c("color"),s("Overview")],singleControls:["type"],source:[{filename:"Source Code",language:"tsx",code:`
+                     import { RadioButton } from "@cocokits/react-components";
+  
+            export const MyComponent = () => {
+              return (
+          <>
+            <% themeComponentConfig.color.values.map((color, index) => { %>
+
+              {/* <%= color %> */}
+              <RadioButton
+                <% if (typeof type !== 'undefined') { %> type='<%= type %>' <% } %>
+                color='<%= color %>'
+                value='YOUR_VALUE'>
+                Radio Button - <%= color %>
+              </RadioButton>
+            <% }) %>
+          </>
+              );
+            }
+
+          `}],controls:[a()]}},render:e=>{var t;return o.jsx(o.Fragment,{children:(t=e.cckControl.themeComponentConfig.color)==null?void 0:t.values.map((n,i)=>o.jsxs(r,{type:e.cckControl.type,color:n,value:i,checked:!0,children:["Radio Button - ",n]},i))})}},M={component:r,title:"UI Components/RadioButton",parameters:{docs:{description:{component:[h].join(`
+`)}},cckAddon:{componentName:"radioButton"}}},D=["Default","Type","Size","Color"];export{L as Color,S as Default,_ as Size,E as Type,D as __namedExportsOrder,M as default};
